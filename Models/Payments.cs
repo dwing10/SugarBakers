412df7e4673e7e7d4f88054478ad83d778aa092b
Devin Wing <wingd@nmc1994.onmicrosoft.com>
2020-11-07 16:47:19 -0500

    Add project files.

---
+++ Models/Payments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SugarBakers.Models
+{
+    public partial class Payments
+    {
+        public int CustomerId { get; set; }
+        public DateTime PmtDate { get; set; }
+        public decimal? Amt { get; set; }
+
+        public virtual Customers Customer { get; set; }
+    }
+}
